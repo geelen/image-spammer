9aa8c270654df07ea0ec6382a1375aea9eee44c3
Glen Maddern <glen@cloudflare.com>
2021-07-02 16:01:31 +0100

    3000px

--- download.rb
+++ download.rb
@@ -10,7 +10,7 @@ require 'uri'
   path = URI(url).path
   file = File.basename(path)
 
-  `curl "#{url}" -o dist/#{file}.jpg`
+  `curl "#{url.replace(/1080/,'3000')}" -o dist/#{file}.jpg`
 end
 
 files = `ls dist`.split("\n")
