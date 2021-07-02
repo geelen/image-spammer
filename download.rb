8db33033fc365c21849af1f7dbe17ce0141ddd28
Glen Maddern <glen@cloudflare.com>
2021-07-02 15:32:01 +0100

    actually downloading and rendeirng a HTML

--- download.rb
+++ download.rb
@@ -2,8 +2,13 @@
   rando = `curl -sI https://source.unsplash.com/random`
   lines = rando.split("\n").map(&:strip)
   line = lines.find { |l| l =~ /^Location:/ }
-  puts line
   _, url, search = line.split(/[ ?]/)
 
-  `cd dist && curl "#{url}"`
+  puts url
+  path = URI(url).path
+
+  `curl "#{url}" -o dist#{path}.jpg`
 end
+
+files = `ls dist`.split("\n")
+File.write('dist/index.html', files.map { |f| "<img src='/#{f}' style='width: 40vw;'>" }.join())
